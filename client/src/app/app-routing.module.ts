07c8b0ed00f43e02a3f06d50b6897861db0e8f99
ankopein <ankopein@edu.aau.at>
2020-01-13 10:08:27 +0100

    Add clientside routing from navbar to login page
    needed later to test the login feature.

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,10 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {SiteComponent} from './site/site.component';
+import {JsongalleryComponent} from "./jsongallery/jsongallery.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'login',
+    component: SiteComponent
+  },
+  {
+    path: 'gallery',
+    component: JsongalleryComponent
+  }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {enableTracing: true})],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
