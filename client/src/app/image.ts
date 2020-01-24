50153684009c5a4e490ed6255a8436ea79508968
chwuggenig <chwuggenig@edu.aau.at>
2020-01-24 19:54:33 +0100

    first version of tag displaying working

--- client/src/app/image.ts
+++ client/src/app/image.ts
@@ -3,7 +3,7 @@ export class Image {
     data_big: string;
     data_small: string;
     desc: string;
-    constructor(id:string, data_big: string, data_small: string, desc: string){
+    constructor(id: string, data_big: string, data_small: string, desc: string) {
         this.id = id;
         this.data_big = data_big;
         this.data_small = data_small;
